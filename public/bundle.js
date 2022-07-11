e5e96eee3f52f0cde9b39f2150c17f6cf2e0ab13
Glitch (combat-io) <none>
2022-07-11 22:40:10 +0000

    🍠🦃 Checkpoint ./public/bundle.js:27836627/613 ./.glitch-assets:27836627/343

--- public/bundle.js
+++ public/bundle.js
@@ -992,6 +992,33 @@ enterGame.addEventListener("click", function(e) {
         })
       }, 50)
     }
+    audio1.pause();
+    audio2.pause();
+    audio3.pause();
+    audio4.pause();
   });
   connect();
 })();
+
+var audio1 = new Audio('audio1.mp3');
+var audio2 = new Audio('audio2.mp3');
+var audio3 = new Audio('audio3.mp3');
+var audio4 = new Audio('audio4.mp3');
+// var audioArray = [audio1, audio2, audio3, audio4];
+
+audio1.play();
+// audio1.loop = Infinity;
+
+// loop audio
+audio1.addEventListener('ended', (event) => {
+  audio2.play();
+});
+audio2.addEventListener('ended', (event) => {
+  audio3.play();
+});
+audio3.addEventListener('ended', (event) => {
+  audio4.play();
+});
+audio4.addEventListener('ended', (event) => {
+  audio1.play();
+});
