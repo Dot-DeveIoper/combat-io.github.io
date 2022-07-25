ccfaef0fad6f4085e90820c58e9b2c88c260bf20
Glitch (combat-io) <none>
2022-07-25 20:32:14 +0000

    🏠🎀 Checkpoint ./public/bundle.js:39200855/140

--- public/bundle.js
+++ public/bundle.js
@@ -655,7 +655,9 @@ function setSkin(num) {
     ctx.drawImage(img, ob.xOffset, ob.yOffset, ob.scale, ob.scale);
     ctx.restore();
   }
+  ///mapSize - myPlayer.y + canvas.height / 2 - snowHeight - 3000 - 1500,
   function drawTree(x, y, rot, id) {
+      console.log(x,y);
     var tree = trees.find((x) => x.id == id);
     var img = "";
     if (id === 0 && myPlayer.y >= 20 && myPlayer.y <= 955) {
