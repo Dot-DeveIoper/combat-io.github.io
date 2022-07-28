516b5542dc4b91e9d6034eb072c1ac262cec2818
Glitch (combat-io) <none>
2022-07-28 01:53:02 +0000

    🍓📹 Checkpoint ./server.js:39200855/157

--- server.js
+++ server.js
@@ -709,7 +709,7 @@
             var obj = weapons.find((x) => x.id == socket.player.weapon);
             if (!obj) return;
 
-            var reqFood = obj.cost.food || 0;
+            var reqFood = obj.cost.wood > 1000 ? Math.ceil(obj.cost.wood / 1000) + "k" : obj.cost.wood || 0;
             var reqWood = obj.cost.wood || 0;
             var reqStone = obj.cost.stone || 0;
             var reqRuby = obj.cost.Ruby || 0;
