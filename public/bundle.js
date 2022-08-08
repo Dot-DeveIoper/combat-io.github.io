5f401b54bc53e30f85dd4704aaeaa6abba218696
Glitch (combat-io) <none>
2022-08-08 13:29:15 +0000

    🌆🗾 Checkpoint ./public/bundle.js:39200855/2403

--- public/bundle.js
+++ public/bundle.js
@@ -1007,17 +1007,25 @@ function setSkin(num) {
       return m(leaderboard[num].gold,
         1);
     }
-    function m(n, d) {
-      if (n > 1000) {
-        (x = ("" + n).length),
-        (p = Math.pow),
-        (d = p(10, d));
-        x -= x % 3;
-        return Math.round((n * d) / p(10, x)) / d + " kMBT"[x / 3];
-      } else {
-        return n;
-      }
+    function m(num, digits) {
+  var si = [
+    { value: 1, symbol: "" },
+    { value: 1E3, symbol: "k" },
+    { value: 1E6, symbol: "M" },
+    { value: 1E9, symbol: "G" },
+    { value: 1E12, symbol: "T" },
+    { value: 1E15, symbol: "P" },
+    { value: 1E18, symbol: "E" }
+  ];
+  var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var i;
+  for (i = si.length - 1; i > 0; i--) {
+    if (num >= si[i].value) {
+      break;
     }
+  }
+  return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
 
     if (leaderboard[0]) {
       document.getElementById("players").innerHTML =
