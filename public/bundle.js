411d7f5875da20d256e246763f0ff8fb0c019a0c
Glitch (combat-io) <none>
2022-08-08 13:41:03 +0000

    🔮🔋 Checkpoint ./public/bundle.js:39200855/104

--- public/bundle.js
+++ public/bundle.js
@@ -1012,10 +1012,7 @@ function setSkin(num) {
     { value: 1, symbol: "" },
     { value: 1E3, symbol: "k" },
     { value: 1E6, symbol: "M" },
-    { value: 1E9, symbol: "G" },
-    { value: 1E12, symbol: "T" },
-    { value: 1E15, symbol: "P" },
-    { value: 1E18, symbol: "E" }
+    { value: 1E9, symbol: "B" }
   ];
   var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
   var i;
