559ea646d2500cd49ae52a76d2ca080823dfe1c5
Glitch (combat-io) <none>
2022-08-19 05:52:08 +0000

    🍍👮 Checkpoint ./server.js:46296953/84

--- server.js
+++ server.js
@@ -857,7 +857,7 @@
           var skin;
           try {
             name = msg[1][0].name.replace(/[^a-z0-9]/gi, "").slice(0, 15) || "Combat.io";
-            skin = socket.player.skin || 4;
+            skin = msg[1][0].skin || 0;
           } catch (err) {
             socket.close(1012, "Buffer missing");
           }
