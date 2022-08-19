fb8bd584c6f6abb949b871939389fd15dc2f1f3b
Glitch (combat-io) <none>
2022-08-19 14:42:17 +0000

    📚🙉 Checkpoint ./server.js:39200855/10

--- server.js
+++ server.js
@@ -871,7 +871,7 @@
           if (!players.find((x) => x.sid == socket.player.sid))
             players.push(socket.player);
           socket.send(msgpack.encode(["1", [socket.player.sid]]));
-          socket.send(msgpack.encode(["w", [socket.player.weapons]]));
+          socket.send(msgpack.encode(["w", [socket.player.weapons - 1]]));
           break;
         case "Hd":
           if (!msg[1][0]) {
