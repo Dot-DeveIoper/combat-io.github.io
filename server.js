87a9a428d6ce26f738acbba6fa786a94473e4f2d
Glitch (combat-io) <none>
2022-08-21 18:04:50 +0000

    💃🌝 Checkpoint ./server.js:46296953/913

--- server.js
+++ server.js
@@ -978,12 +978,7 @@
             socket.close();
             return false;
           }
-        var e = msg[1][0];
-        var BadWords = ["fuck", "shit", "bitch"];
-        for(let words = 0; words < BadWords.length; words++) {
-          e.replaceAll(BadWords[words], "NaughtyWord")
-        }
-        socket.player.chat = e.slice(0, 30);
+        socket.player.chat = msg[1][0].slice(0, 30);
         clearTimeout(chatMessages[socket.player.sid])
         chatMessages[socket.player.sid] = setTimeout(() => {
           socket.player.chat = null;
