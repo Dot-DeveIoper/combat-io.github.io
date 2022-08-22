86867635a99d4769792661fd3e79e2d9ab61de52
Glitch (combat-io) <none>
2022-08-22 01:13:52 +0000

    🍇🎓 Checkpoint ./public/bundle.js:39200855/16

--- public/bundle.js
+++ public/bundle.js
@@ -773,8 +773,8 @@
       src: "https://cdn.glitch.global/069d62dd-5ac4-4928-9200-7250f0cc75c3/WoodWall.png?v=1660858007447",
       scale: 110,
       img: new Image(),
-      xOffset: -50,
-      yOffset: -50,
+      xOffset: 0,
+      yOffset: 0,
     },
     {
       id: 4,
