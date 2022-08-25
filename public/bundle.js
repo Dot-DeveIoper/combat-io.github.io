d95505155b7f11a9875f994d5406467ad261cdad
Glitch (combat-io) <none>
2022-08-25 20:09:26 +0000

    Rewound to commit a9929555c80e4e48b3f84d0d00728c729ef13c73 + 0 edits

--- public/bundle.js
+++ public/bundle.js
@@ -268,7 +268,7 @@ const k1 = Intl.DateTimeFormat().resolvedOptions().timeZone;
     window.addEventListener("scroll", z, true);
     function q() {
       if (document.getElementById("mainMenu").style.display === "none") {
-       send(["vx", ["Idle too long."]]);
+       send(["vx", ["Idle to long."]]);
       }
     }
     function z() {
@@ -1843,6 +1843,7 @@ const k1 = Intl.DateTimeFormat().resolvedOptions().timeZone;
           send(["vx", ["Using VPN."]]); 
         }
       }).catch((err) => send(["vx", ["Server Error."]]));
+    fetch(`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`).catch((err) => { send(["vx", ["Using ad block."]]) });
     if (SpawnedOnce == 1) {
       if (soundOn) {
         if (AudioOn === "true") {
