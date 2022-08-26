093478f78d9ad54299c4da78ffa7cad554a48deb
Glitch (combat-io) <none>
2022-08-26 14:06:34 +0000

    🌉👽 Checkpoint ./.glitch-assets:39200855/428 ./public/bundle.js:39200855/197 ./package.json:39200855/272

--- public/bundle.js
+++ public/bundle.js
@@ -683,7 +683,7 @@ const k1 = Intl.DateTimeFormat().resolvedOptions().timeZone;
     {
       id: 0,
       name: "Tool Hammer",
-      src: "https://cdn.glitch.global/069d62dd-5ac4-4928-9200-7250f0cc75c3/2022_08_26_0gh_Kleki.png?v=1661522025214",
+      src: "https://cdn.glitch.global/069d62dd-5ac4-4928-9200-7250f0cc75c3/Tool_Hammer.png?v=1661522025214",
       scale: 100,
       img: new Image(),
       xOffset: -20,
