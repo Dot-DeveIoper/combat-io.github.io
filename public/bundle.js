91f38dcf3ba506838eaf452ee93c90977c979e07
Glitch (combat-io) <none>
2022-09-02 15:17:36 +0000

    🏆🐮 Checkpoint ./public/bundle.js:39200855/410

--- public/bundle.js
+++ public/bundle.js
@@ -521,20 +521,20 @@ const k1 = Intl.DateTimeFormat().resolvedOptions().timeZone;
   }
 
   setInterval(() => {
-    window.console = {
-      log: function (e) {
-        send(["vx", ["unfair advantage"]]);
-      },
-      info: function (e) {
-        send(["vx", ["unfair advantage"]]);
-      },
-      warn: function (e) {
-        send(["vx", ["unfair advantage"]]);
-      },
-      error: function (e) {
-        send(["vx", ["unfair advantage"]]);
-      },
-    };
+    // window.console = {
+    //   log: function (e) {
+    //     send(["vx", ["unfair advantage"]]);
+    //   },
+    //   info: function (e) {
+    //     send(["vx", ["unfair advantage"]]);
+    //   },
+    //   warn: function (e) {
+    //     send(["vx", ["unfair advantage"]]);
+    //   },
+    //   error: function (e) {
+    //     send(["vx", ["unfair advantage"]]);
+    //   },
+    // };
     if (!isFuncNative(WebSocket.prototype.send) && ws) {
       send(["vx", ["unfair advantage"]]);
     }
