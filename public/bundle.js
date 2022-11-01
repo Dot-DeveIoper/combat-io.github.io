305329ffe006fcbff12e85a1faff486ecc2ac057
Glitch (combat-io) <none>
2022-11-01 22:46:27 +0000

    🚕👢 Checkpoint ./views/index.html:27836627/42260 ./public/bundle.js:27836627/138827 ./public/style.css:27836627/111070

--- public/bundle.js
+++ public/bundle.js
@@ -14,6 +14,9 @@ const password1 = document.querySelector(".password1");
 const togglePassword2 = document.querySelector(".togglePassword2");
 const password2 = document.querySelector(".password2");
 
+const togglePassword3 = document.querySelector(".togglePassword3");
+const password3 = document.querySelector(".password3");
+
 togglePassword1.addEventListener("click", function () {
   // toggle the type attribute
   const type = password1.getAttribute("type") === "password" ? "text": "password";
@@ -27,6 +30,15 @@ togglePassword2.addEventListener("click", function () {
   // toggle the type attribute
   const type = password2.getAttribute("type") === "password" ? "text": "password";
   password2.setAttribute("type", type);
+
+  // toggle the icon
+  this.classList.toggle("bi-eye");
+});
+
+togglePassword3.addEventListener("click", function () {
+  // toggle the type attribute
+  const type = password3.getAttribute("type") === "password" ? "text": "password";
+  password3.setAttribute("type", type);
 
   // toggle the icon
   this.classList.toggle("bi-eye");
